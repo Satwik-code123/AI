a776fa9bf91c7dda16f7c0b435f4b054b9ae01e8
Satwik-code123 <satwikkhared73@gmail.com>
2025-01-15 18:19:05 +0700

    L1

---
+++ Lesson 1/L1.py
@@ -0,0 +1,13 @@
+print("Hello and welcome to the bot and I'm bot L1!")
+name = input("What is your name?\n")
+
+print(f"Hello {name}, So how are you feeling today?")
+feeling = input()
+
+if feeling == "great":
+    print(f"Lesgooo {name}, you are feeling great!! Glad to hear so!")
+elif feeling == "bad":
+    print(f"Oh noo!, You are feeling sad :(, try listening to some music :)")
+else:
+    print(f"I see sometimes it's hard to put feelings into words")
+
